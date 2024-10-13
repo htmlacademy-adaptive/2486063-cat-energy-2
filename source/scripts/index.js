67d517beb4e964d0d507d402fa4d769ad4cb9165
AllCash412 <nekris13@gmail.com>
2024-10-13 23:40:27 +0300

    подключает скрипт для открытия мобильной навигации

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const navigation = document.querySelector('.navigation');
+const navToggle = document.querySelector('.header__toggle');
+
+navigation.classList.remove('navigation--no-js');
+
+navToggle.addEventListener('click', () => {
+  if (navigation.classList.contains('navigation--closed')) {
+    navigation.classList.remove('navigation--closed');
+    navigation.classList.add('navigation--opened');
+    navToggle.setAttribute('aria-label', 'закрыть меню');
+  } else {
+    navigation.classList.add('navigation--closed');
+    navigation.classList.remove('navigation--opened');
+    navToggle.setAttribute('aria-label', 'открыть меню');
+  }
+});
